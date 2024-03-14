36c83c3c402a8146c9981a1a46734141edae3699
HaseefMuhammed <haseefmuhammedpc@gmail.com>
2024-03-14 23:31:02 +0530

    Final file

---
+++ shop@hf/script.js
@@ -0,0 +1,15 @@
+ScrollReveal().reveal('.card', {
+    scale: 0.85
+});
+
+ScrollReveal().reveal('.message',{
+    scale: 0.85
+});
+
+console.log("Script Ideled")
+console.log("Current_Verson_03_Time_:_9:14pm_Date_:_21-2-2024_Developed_By_:_Haseef_Muhammed_PC")
+
+function meet(){
+    alert("No meeting available...")
+    // window.location.href="https://meet.google.com/"
+}
